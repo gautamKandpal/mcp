0d2b3e002dcf88c9e23678e0881e3e06884cc2ac
gautamkandpal <gkandpal33@gmail.com>
2025-08-27 23:52:35 +0530

    feat:implemented resource and prompt

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
 import fs from "node:fs/promises";
+import { title } from "node:process";
 
 // Create an MCP server
 const server = new McpServer({
@@ -51,6 +55,122 @@ server.tool(
         ],
       };
     }
+  }
+);
+
+server.resource(
+  "users",
+  "users://all",
+  {
+    description: "Get all users from the database",
+    title: "Users",
+    mineType: "application/json",
+  },
+  async (uri) => {
+    const users = await import("./data/user.json", {
+      with: { type: "json" },
+    }).then((m) => m.default);
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(users),
+          mimeType: "application/json",
+        },
+      ],
+    };
+  }
+);
+
+server.resource(
+  "user-details",
+  new ResourceTemplate("users://{userId}/profile", { list: undefined }),
+  {
+    description: "Get a user's detials from the database",
+    title: "Users Details",
+    mineType: "application/json",
+  },
+  async (uri, { userId }) => {
+    const users = await import("./data/user.json", {
+      with: { type: "json" },
+    }).then((m) => m.default);
+
+    const user = users.find((u) => u.id === parseInt(userId as string));
+
+    if (user == null) {
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            text: JSON.stringify({ error: "User not found" }),
+            mimeType: "application/json",
+          },
+        ],
+      };
+    }
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(user),
+          mimeType: "application/json",
+        },
+      ],
+    };
+  }
+);
+
+server.prompt(
+  "genrate-fake-user",
+  "Genrate fake user based on the a given name",
+  {
+    name: z.string(),
+  },
+  async ({ name }) => {
+    //insert only if name is not in user.json
+    const users = await import("./data/user.json", {
+      with: { type: "json" },
+    }).then((m) => m.default);
+
+    const existingUser = users.find(
+      (u) => u.name.trim().toLowerCase() === name.trim().toLowerCase()
+    );
+    if (existingUser) {
+      return {
+        message: [
+          {
+            role: "user",
+            content: {
+              type: "text",
+              text: `User already exists: ${JSON.stringify(existingUser)}`,
+            },
+          },
+        ],
+      };
+    }
+
+    const fakeUser = {
+      name,
+      email: `${name.toLowerCase()}.patel92@gmail.com`,
+      address: "742 Evergreen Terrace, Springfield, IL",
+      phone: "312-555-0198",
+    };
+    users.push(fakeUser);
+    await fs.writeFile("./src/data/user.json", JSON.stringify(users, null, 2));
+
+    return {
+      messages: [
+        {
+          role: "user",
+          content: {
+            type: "text",
+            text: `Genrate a fake user with the name ${name}. The user should have a realistic email, address, and phone number.`,
+          },
+        },
+      ],
+    };
   }
 );
 
