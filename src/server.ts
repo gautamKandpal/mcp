c6995e7945f927b35e10ef77d7b9c87bb030ab22
gautamkandpal <gkandpal33@gmail.com>
2025-08-27 18:48:09 +0530

    feat: created tools for mcp to createAUser

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import { z } from "zod";
+import fs from "node:fs/promises";
+
 // Create an MCP server
 const server = new McpServer({
   name: "myTestServer",
@@ -12,18 +14,62 @@ const server = new McpServer({
   },
 });
 
-server.tool("create-user","Creates a new user in the databse ", {
-    name: z.string(), 
-    email: z.string(), 
+server.tool(
+  "create-user",
+  "Creates a new user in the databse ",
+  {
+    name: z.string(),
+    email: z.string(),
     address: z.string(),
-    phone: z.string()
-},{
+    phone: z.string(),
+  },
+  {
     title: "Create User",
     readOnlyHint: false,
     destructiveHint: false,
     idempotentHint: false,
-    openWorldHint: true
-}, async (params) =>  return{})
+    openWorldHint: true,
+  },
+  async (params) => {
+    try {
+      const id = await createUser(params);
+      return {
+        content: [
+          {
+            type: "text",
+            text: `User ${id} created successfully`,
+          },
+        ],
+      };
+    } catch {
+      return {
+        content: [
+          {
+            type: "text",
+            text: "Failed to save user",
+          },
+        ],
+      };
+    }
+  }
+);
+
+async function createUser(user: {
+  name: string;
+  email: string;
+  address: string;
+  phone: string;
+}): Promise<string> {
+  const users = await import("./data/user.json", {
+    with: { type: "json" },
+  }).then((m) => m.default);
+
+  const id = users.length + 1;
+  users.push({ id, ...user });
+
+  await fs.writeFile("./src/data/user.json", JSON.stringify(users, null, 2));
+  return id.toString();
+}
 
 async function main() {
   // Start receiving messages on stdin and sending messages on stdout
